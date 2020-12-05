241b7fc9439c42e546519ed460515769ceeae688
mcnabbno <mcnabbno@sheridancollege.ca>
2020-12-05 17:49:22 -0500

    Added BlackjackPlayer class. 	- Supports drawing into the player hand 	- Can check the handValue of the players hand 	- Can adjust the value of ACES(11 or 1) in the player hand to avoid 	  busting too early

---
+++ SYST17796_Group3_Deliverable3/src/ca/sheridancollege/project/BlackjackPlayer.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.project;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Noah
+ */
+public class BlackjackPlayer extends Player{
+    private ArrayList<Card> hand = new ArrayList<>();
+    
+    private int handValue, scoreCount;
+    
+    public BlackjackPlayer(String name){
+        super(name);
+    }
+    
+    public void resetHand(){
+        this.hand.clear();
+    }
+    
+    public void calcHandValue(){
+        //Adds up the total Value of the Hand
+        for(Card c : this.hand){
+            CardFor52Deck d = (CardFor52Deck)c;
+            this.handValue = handValue + d.getValue().getCardVal();
+        }
+        //Handles changing Aces to have a value of 1 instead of 11 to avoid bust
+        if(handValue > 21){
+            for(Card c : this.hand){
+                CardFor52Deck d = (CardFor52Deck)c;
+                if(d.getValue() == CardFor52Deck.Value.ACE && handValue > 21){
+                    this.handValue = handValue -10;
+                }
+            }
+        }
+    }
+    
+    @Override
+    public void play() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public ArrayList<Card> getHand() {
+        return hand;
+    }
+
+    public void setHand(ArrayList<Card> hand) {
+        this.hand = hand;
+    }
+
+    public int getHandValue() {
+        return handValue;
+    }
+
+    public void setHandValue(int handValue) {
+        this.handValue = handValue;
+    }
+
+    public int getScoreCount() {
+        return scoreCount;
+    }
+
+    public void setScoreCount(int scoreCount) {
+        this.scoreCount = scoreCount;
+    }
+    
+    
+    
+}
--- SYST17796_Group3_Deliverable3/src/ca/sheridancollege/project/CardFor52Deck.java
+++ SYST17796_Group3_Deliverable3/src/ca/sheridancollege/project/CardFor52Deck.java
@@ -19,8 +19,19 @@ public class CardFor52Deck extends Card {
     };
 
     public enum Value {
-        ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE,
-        TEN, JACK, QUEEN, KING
+        ACE(11), TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7),
+        EIGHT(8), NINE(9), TEN(10), JACK(10), QUEEN(10), KING(10);
+
+        private final int cardVal;
+
+        private Value(int cardVal) {
+            this.cardVal = cardVal;
+        }
+
+        public int getCardVal() {
+            return this.cardVal;
+        }
+
     };
 
     public CardFor52Deck(Value value, Suit suit) {
